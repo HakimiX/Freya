b6c9e4a7817f79c1b0f72b468a2613e35a803ba9
HakimiX <mustafa.hakimi@outlook.dk>
2022-06-11 12:55:56 +0200

    update install script

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
 
-# Options
-# Install dependencies (MacOS)
-# $ ./install-dependencies.sh --mac
-# Install dependencies (Ubuntu)
-# $ ./install-dependencies.sh --ubuntu
-
 OS=$1
 
 if [ "$OS" == "--mac" ]; then
   echo "Installing dependencies for MacOS using homebrew..."
-  #brew install leiningen
-  #brew install terraform@1.2
-  #echo 'export PATH="/usr/local/opt/terraform@1.2/bin:$PATH"' >> ~/.zshrc
-  #brew install awscli
+  brew install leiningen
+  brew install terraform@1.2
+  echo 'export PATH="/usr/local/opt/terraform@1.2/bin:$PATH"' >> ~/.zshrc
+  brew install awscli
 elif [ "$OS" == "--ubuntu" ]; then
   echo "Installing dependencies for Ubuntu using apt..."
   # todo...
-
+elif [ "$OS" == "" ]; then
+  echo "Please provide your OS, i.e. --mac or --ubuntu"
 fi
 
 exit 0
