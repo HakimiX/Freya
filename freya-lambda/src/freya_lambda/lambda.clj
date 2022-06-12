41f418a08b178ca881980a396e7e93c09e176ba3
HakimiX <mustafa.hakimi@outlook.dk>
2022-06-12 18:46:03 +0200

    parse incoming event and add initial handler function

--- freya-lambda/src/freya_lambda/lambda.clj
+++ freya-lambda/src/freya_lambda/lambda.clj
@@ -1,8 +1,15 @@
 (ns freya-lambda.lambda
   "Doc-string here..."
-  (:require [clojure.tools.logging :as log]
+  (:require [cheshire.core :as json]
+            [clojure.tools.logging :as log]
             [uswitch.lambada.core :refer [deflambdafn]]))
+
+(defn handle-event [event]
+  (log/info :message "Handling event" :event event))
 
 (deflambdafn freya-lambda.lambda.handler
   [in out ctx]
-  (log/info :message "Incoming event" :event in))
+  (log/info :message "Incoming event" :event in)
+  (-> (slurp in)
+      (json/parse-string keyword)
+      handle-event))
