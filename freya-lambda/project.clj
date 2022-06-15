07c3bf179151244e06a94b2df3d40fdcec565b1d
HakimiX <mustafa.hakimi@outlook.dk>
2022-06-15 12:27:13 +0200

    remove licens

--- freya-lambda/project.clj
+++ freya-lambda/project.clj
@@ -1,7 +1,5 @@
 (defproject freya-lambda "0.1.0-SNAPSHOT"
   :description "AWS Lambda function"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [cheshire "5.10.2"]
                  [com.amazonaws/aws-java-sdk-s3 "1.12.237"]
